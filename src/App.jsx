18a71708f01c508084598e74d7882650a984b135
ManavR123 <manav.rathod@berkeley.edu>
2020-07-18 00:41:37 -0400

    WIP - centering heart eyes

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,24 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'center',
   },
 
+  blush: {
+    overflow: 'visible',
+  },
+
+  blushend: {
+    stopColor: 'rgb(255,255,255)',
+    stopOpacity: 0,
+  },
+
+  blushstart: {
+    stopColor: 'rgb(222,93,131)',
+    stopOpacity: 1,
+  },
+
   face: {
     display: 'flex',
-    height: '5rem',
+    height: '25%',
     justifyContent: 'center',
-    padding: '20vh 0 20vh 0',
   },
 
   eye: {
@@ -25,8 +38,8 @@ const useStyles = makeStyles(() => ({
   },
 
   eyeBox: {
-    height: '2.5rem',
-    width: '2.5rem',
+    height: 300,
+    width: 0,
   },
 
   mouth: {
@@ -94,49 +107,72 @@ const App = () => {
 
   const handleMouseMove = (event) => {
     const rect = faceRef.current.getBoundingClientRect();
-    setX(event.clientX - rect.left);
-    setY(event.clientY - rect.top);
+    // setX(event.clientX - rect.left);
+    // setY(event.clientY - rect.top);
   };
 
-  const heart = (
+  const heart = (x, y) => (
     <svg className="heart" viewBox="0 0 32 29.6">
       <path
         d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
 c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
+        transform={`translate(${x}, ${y})`}
       />
     </svg>
   );
 
-  const renderEye = (cx, cy) => {
+  const renderEye = (cx, cy, bx, by) => {
     let ecx = cx;
-    if (x >= 0 && x <= 200) {
+    if (x < -200) {
+      ecx = cx - 20;
+    } else if (x >= -200 && x <= 200) {
       ecx = cx + x / 10;
-    } else if (x >= 200) {
+    } else if (x > 200) {
       ecx = cx + 20;
     }
 
     return (
-      <div className={classes.eyeBox}>
+      <>
         <svg className={classes.eye}>
-          <circle cx={ecx} cy={Math.min(cy + y / 10, 25)} r={25} fill="#333" />
+          <circle cx={ecx} cy={Math.min(cy + y / 10, 20)} r={25} fill="#333" />
         </svg>
-      </div>
+        {blush(bx, by)}
+      </>
+    );
+  };
+
+  const blush = (cx, cy) => {
+    return (
+      <svg height="150" width="500" className={classes.blush}>
+        <defs>
+          <radialGradient
+            id="grad1"
+            cx="50%"
+            cy="50%"
+            r="50%"
+            fx="50%"
+            fy="50%"
+          >
+            <stop offset="0%" className={classes.blushstart} />
+            <stop offset="100%" className={classes.blushend} />
+          </radialGradient>
+        </defs>
+        <ellipse cx={cx} cy={cy} rx="85" ry="55" fill="url(#grad1)" />
+      </svg>
     );
   };
 
   const face = (
-    <div
-      className={classes.face}
-      onClick={() => {
-        setUseHeart(!useHeart);
-      }}
-      ref={faceRef}
-    >
-      {useHeart ? heart : renderEye(0, 0)}
-      <div className={classes.mouth}>
-        <div className={classes.happy} />
+    <div className={classes.face} ref={faceRef}>
+      <div className={classes.eyeBox}>
+        {useHeart ? heart(-17, -40) : renderEye(0, -30, -50, -110)}
       </div>
-      {useHeart ? heart : renderEye(25, 0)}
+      <div className={classes.mouth}>
+        <div className={classes.smile} />
+      </div>
+      <div className={classes.eyeBox}>
+        {useHeart ? heart(-17, -40) : renderEye(0, -30, 50, -110)}
+      </div>
     </div>
   );
 
@@ -146,6 +182,7 @@ c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
         className={classes.background}
         onClick={() => {
           setFullscreen(!fullscreen);
+          setUseHeart(!useHeart);
         }}
         onMouseMove={handleMouseMove}
       >
